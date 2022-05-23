7b9926ec265c72b0790b46db110997106855498a
saintholy <39052604+saintholy@users.noreply.github.com>
2022-05-23 04:52:21 +0000

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,10 +1,4 @@
 #include "main.h"
-/**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
- */
 int _printf(const char *format, ...)
 {
 	int printed_chars;
@@ -12,6 +6,15 @@ int _printf(const char *format, ...)
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
 		{NULL, NULL}
 	};
 	va_list arg_list;
