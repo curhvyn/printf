afc21d7c0d865d9111d11e608bf774d13148df5f
saintholy <39052604+saintholy@users.noreply.github.com>
2022-05-23 02:50:22 +0000

    Update printf.c

--- printf.c
+++ printf.c
@@ -12,8 +12,6 @@ int _printf(const char *format, ...)
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
 		{NULL, NULL}
 	};
 	va_list arg_list;
